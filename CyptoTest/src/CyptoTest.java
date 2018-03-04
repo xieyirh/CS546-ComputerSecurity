34485e767ff269141c77bb70f35fb4568f84ee2d
yxie <yxie@localhost>
2018-03-03 17:16:08 -0700

    contributor 2 first commit

--- CyptoTest/src/CyptoTest.java
+++ CyptoTest/src/CyptoTest.java
@@ -3,6 +3,7 @@ public class CyptoTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Cyptographic World!");
+		System.out.println("Send from another contributor.");
 	}
 
 }
