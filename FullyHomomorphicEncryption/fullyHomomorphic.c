2c793b2cdff570ecf72108ae0c7fc83634ca9233
Yi Xie <xie.yi.me@gmail.com>
2018-03-23 11:42:33 -0600

    FHEv1 -k  done

--- FullyHomomorphicEncryption/fullyHomomorphic.c
+++ FullyHomomorphicEncryption/fullyHomomorphic.c
@@ -23,7 +23,11 @@ void genPrime(BIGD p, size_t keySize){
         bdRandomBits(p, keySize);
     }
 }
-
+/**
+ * write the prime number to a file
+ * @param p: prime number
+ * @param file: keyfile
+ */ 
 void writePrimeHexStringToFile(BIGD p, FILE* file){
     char* primeString;
     size_t nChars = bdConvToHex(p,NULL, 0);
@@ -32,6 +36,36 @@ void writePrimeHexStringToFile(BIGD p, FILE* file){
     fputs(primeString,file);
     fputs("\n", file);
     free(primeString);
+}
+
+/**
+ * Generate equality test key
+ * @param g: equality test key
+ * @param p3
+ * @param t: t = q x p3
+ * @param keysize
+ */ 
+void genEqualityTestKey(BIGD g, BIGD p3, BIGD t, size_t keySize){
+    BIGD expo, h, p3Decrement, bigTwo, bigOne;
+    expo = bdNew();
+    h = bdNew();
+    p3Decrement = bdNew();
+    bigTwo = bdNew();
+    bigOne = bdNew();
+    
+    bdSetShort(bigOne, 1);
+    bdSetShort(bigTwo, 2);
+
+    bdMultiply_s(p3Decrement, p3, bigOne);
+    bdDecrement(p3Decrement);
+    bdMultiply_s(expo, p3Decrement, bigTwo);
+    bdRandomBits(h, 2*keySize);
+
+    //printf("The random number h = ");
+    //bdPrint(h, 0x1);
+
+
+    bdModExp(g, h, expo,t);
 }
 
 /**
@@ -68,6 +102,13 @@ void keyGen(size_t keySize, char* keyFile){
     bdMultiply_s(n, p1,p2);
     bdMultiply_s(t, q, p3);
 
+    BIGD g1, g2;
+    g1 = bdNew();
+    g2 = bdNew();
+    genEqualityTestKey(g1, p3, t, keySize);
+    genEqualityTestKey(g2, p3, t, keySize);
+    
+
     FILE* file = fopen(keyFile, "w+");
     if (file == NULL){
         exit(EXIT_FAILURE);
@@ -76,36 +117,45 @@ void keyGen(size_t keySize, char* keyFile){
     writePrimeHexStringToFile(p2, file);
     writePrimeHexStringToFile(p3, file);
     writePrimeHexStringToFile(q, file);
+    writePrimeHexStringToFile(g1, file);
+    writePrimeHexStringToFile(g2, file);
     writePrimeHexStringToFile(n, file);
     writePrimeHexStringToFile(t, file);
 
-    fclose(file);
 
+    fclose(file);
+/*
     printf("The prime number p1 = ");
     bdPrint(p1, 0x1);
-    printf("The size of p1 is:%ld\n",bdBitLength(p1) );
+    //printf("The size of p1 is:%ld\n",bdBitLength(p1) );
 
     printf("The prime number p2 = ");
     bdPrint(p2, 0x1);
-    printf("The size of p2 is:%ld\n",bdBitLength(p2) );
+    //printf("The size of p2 is:%ld\n",bdBitLength(p2) );
 
     printf("The prime number p3 = ");
     bdPrint(p3, 0x1);
-    printf("The size of p3 is:%ld\n",bdBitLength(p3) );
+    //printf("The size of p3 is:%ld\n",bdBitLength(p3) );
     printf("The prime number q = ");
     bdPrint(q, 0x1);
-    printf("The size of q is:%ld\n",bdBitLength(q) );
+    //printf("The size of q is:%ld\n",bdBitLength(q) );
+
+    printf("The prime number g1 = ");
+    bdPrint(g1, 0x1);
+    //printf("The size of g1 is:%ld\n",bdBitLength(g1) );
+
+    printf("The prime number g2 = ");
+    bdPrint(g2, 0x1);
+    //printf("The size of g2 is:%ld\n",bdBitLength(g2) );
 
     printf("The number n = ");
     bdPrint(n, 0x1);
-    printf("The size of n is:%ld\n",bdBitLength(n) );
+    //printf("The size of n is:%ld\n",bdBitLength(n) );
 
     printf("The number t = ");
     bdPrint(t, 0x1);
-    printf("The size of t is:%ld\n",bdBitLength(t) );
-    
-    
-
+    //printf("The size of t is:%ld\n",bdBitLength(t) ); 
+*/
     bdFree(&p1);
     bdFree(&p2);
     bdFree(&p3);
@@ -113,7 +163,6 @@ void keyGen(size_t keySize, char* keyFile){
     bdFree(&bigTwo);
     bdFree(&n);
     bdFree(&t);
-    
     
     return;
 }
